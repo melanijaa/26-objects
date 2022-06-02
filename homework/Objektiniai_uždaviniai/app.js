04e3ae5c2c8b35617d074a063f47d709f2605303
Melanija <melanija.sibakovska1@gmail.com>
2022-06-02 10:56:16 +0300

    homework

---
+++ homework/Objektiniai_uždaviniai/app.js
@@ -0,0 +1,14 @@
+//1
+class Kibiras1 {
+    constructor(akmenuKiekis) {
+    }
+    prideti1Akmeni = () => {
+
+    }
+    pridetiDaugAkmenu = (kiekis) => {
+
+    }
+    kiekPririnktaAkmenu = () => {
+        console.log()
+    }
+}
