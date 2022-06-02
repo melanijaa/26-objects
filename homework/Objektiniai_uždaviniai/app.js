39fc0656172dd08905c2ba0e12da0670dc22e2e7
Melanija <melanija.sibakovska1@gmail.com>
2022-06-02 13:05:01 +0300

    class work: static

--- homework/Objektiniai_uždaviniai/app.js
+++ homework/Objektiniai_uždaviniai/app.js
@@ -55,3 +55,82 @@ pinigai.ideti(3);
 pinigai.skaiciuoti();
 pinigai.monetos();
 pinigai.banknotai();
+
+//8
+ class Stikline {
+     static gerimukas = 'pepsi';
+     static registras = [];
+
+     static naujaStikline(t) {
+         return new this(t);
+     }
+
+     static whatType() {
+        console.log('blabla');
+         console.log(this.gerimukas);
+     }
+
+     static visosStiklines (stikline) {
+         this.registras.push(stikline);
+     }
+     
+     constructor(turis) {
+         this.turis = turis;
+         this.kiekis = 0;
+         this.constructor.visosStiklines(this);
+     }
+
+     ipilti(kiekis) {
+         this.kiekis = Math.min(this.turis, this.kiekis + kiekis); // vietoj if'o
+         return this;
+     }
+     ispilti() {
+         const kiek = this.kiekis;
+         this.kiekis = 0;
+         return kiek;
+     }
+     stiklinejeYra() {
+         console.log(`Stiklinėje ${this.turis} ml yra: ${this.kiekis} ${this.constructor.gerimukas}.`)
+     }
+ }
+const s1 = new Stikline(200);
+const s2 = Stikline.naujaStikline(150);
+const s3 = new Stikline(100);
+console.log(s1);
+console.log(Stikline.gerimukas);
+Stikline.whatType();
+console.log(Stikline.registras);
+
+//pvz
+//s2.ipilti(s1.ipilti(80).ispilti());
+
+//chane, duomenu perdavimas
+//klasikinis objektinis programavimas
+s3.ipilti(s2.ipilti(s1.ipilti(80).ispilti()).ispilti());
+Stikline.registras[0].ipilti(40);
+Stikline.registras[1].ipilti(120);
+Stikline.registras[2].ipilti(5);
+
+/*
+//wrong, cause of the positioning
+s1.ipilti(1000);
+s1.stiklinejeYra();
+
+s2.ipilti(s1.ispilti());
+s2.stiklinejeYra();
+
+s3.ipilti(s2.ispilti());
+s3.stiklinejeYra();
+*/
+
+/*
+//positioning matters
+s1.ipilti(1000);
+s2.ipilti(s1.ispilti());
+s3.ipilti(s2.ispilti());
+*/
+
+s1.stiklinejeYra();
+s2.stiklinejeYra();
+s3.stiklinejeYra();
+
