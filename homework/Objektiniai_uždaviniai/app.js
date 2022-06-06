5648fafb477a822fa278521d9772b4b2bde2a75a
Melanija <melanija.sibakovska1@gmail.com>
2022-06-06 12:26:17 +0300

    hw grybai

--- homework/Objektiniai_uždaviniai/app.js
+++ homework/Objektiniai_uždaviniai/app.js
@@ -135,3 +135,39 @@ s1.stiklinejeYra();
 s2.stiklinejeYra();
 s3.stiklinejeYra();
 
+//9
+class Grybas {
+    constructor() {
+        this.valgomas = !this.rand(0, 1);
+        this.sukirmijes = !this.rand(0, 1);
+        this.svoris = this.rand(5, 45);
+    }
+
+    rand(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+}
+
+class Krepsys {
+    constructor() {
+        this.dydis = 500;
+        this.prikrauta = 0;
+    }
+    deti(grybas) {
+        if(grybas.valgomas && !grybas.sukirmijes) {
+            this.prikrauta += grybas.svoris;
+        }
+        return this.prikrauta < this.dydis;
+    }
+}
+
+const Kr = new Krepsys();
+
+let count = 0; //tik vizualizacijai
+while(Kr.deti(new Grybas())){
+    console.log(`grybauju...` + ++count) //tik vizualizacijai
+}
+console.log(Kr);
+
